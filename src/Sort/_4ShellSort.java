05d6fd0427058f4f763bace78d4dafdc519924c8
Lu <ext-yang.lu@here.com>
2017-11-22 11:48:15 -0600

    shellsort

--- src/Sort/_4ShellSort.java
+++ src/Sort/_4ShellSort.java
@@ -1,4 +1,25 @@
 package Sort;
 
-public class _4ShellSort {
+public class _4ShellSort   {
+    public static void main(String[] args) {
+        int[] intArray = {20, 35, -15, 7, 55, 1, 22, -5};
+
+        //gap == 1 for final iteration
+        for (int gap = intArray.length / 2; gap > 0; gap /= 2) {
+            //3, 1
+
+            for (int i = gap; i < intArray.length; i++) {
+                int newElement = intArray[i];
+                int j = i;
+                while (j >= gap && intArray[j - gap] > newElement) {
+                    intArray[j] = intArray[j - gap];
+                    j -= gap;
+                }
+                intArray[j] = newElement;
+            }
+        }
+        for (int i = 0; i < intArray.length; i++){
+            System.out.println(intArray[i]);
+        }
+    }
 }
