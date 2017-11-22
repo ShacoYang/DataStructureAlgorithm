2cfd510d33164cac19a386786a56f88f902f4b0c
Lu <ext-yang.lu@here.com>
2017-11-22 11:48:52 -0600

    recursion mergersort

--- src/Sort/_5Recursion.java
+++ src/Sort/_5Recursion.java
@@ -1,4 +1,29 @@
 package Sort;
 
 public class _5Recursion {
+    public static void main(String[] args) {
+        System.out.println(recursionFactorial(4));
+    }
+
+    // 1! = 1 * 0!
+    // 2! = 2 * 1 = 2 * 1!
+    // 3! = 3 * 2 * 1 = 3 * 2!
+    // 4! = 4 * 3 * 2 * 1 = 4 * 3!
+    // n! = n * (n -1)!
+    public static int recursionFactorial(int num) {
+        if (num ==0 || num ==1){
+            return 1;
+        }
+        return num* recursionFactorial(num -1);
+    }
+    public static int interativeFactorial(int num) {
+        if (num == 0) {
+            return 1;
+        }
+        int factorial = 1;
+        for (int i = 1; i <= num; i++) {
+            factorial *= i;
+        }
+        return factorial;
+    }
 }
--- src/Sort/_6MergeSort.java
+++ src/Sort/_6MergeSort.java
@@ -1,4 +1,43 @@
 package Sort;
 
 public class _6MergeSort {
+
+
+    public static void main(String[] args) {
+        int[] intArray = {20, 35, -15, 7, 55, 1, 22, -5};
+
+        mergeSort(intArray, 0, intArray.length);
+        for (int i = 0; i < intArray.length; i++) {
+            System.out.println(intArray[i]);
+        }
+    }
+    //20, 35, -15, 7, 55, 1, 22, -5
+    public static void mergeSort(int[] input, int start, int end) {
+        if (end - start < 2){
+            return;
+        }
+        int mid = start + (end - start) / 2;
+        mergeSort(input, start, mid);
+        mergeSort(input, mid, end);
+        merge(input, start, mid, end);
+
+    }
+    public static void merge(int[] input, int start, int mid, int end){
+        if (input[mid - 1] <= input[mid]){
+            return;
+        }
+        int i = start;
+        int j = mid;
+        int tempIndex = 0;
+
+        int[] temp = new int[end - start];
+        //no need to worry handling any left over (right part)--> copy back
+        while (i < mid && j < end) {
+            temp[tempIndex++] = input[i] <= input[j] ?input[i++] :input[j++];
+        }
+        //handling the remaining elements
+        System.arraycopy(input, i, input, start + tempIndex, mid - i);
+        System.arraycopy(temp, 0, input, start, tempIndex);
+    }
+
 }
